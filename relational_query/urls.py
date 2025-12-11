69aac1f75aee416132f79c3a67093e915bbe394e
Mostafa Raihan <raihan.invite@gmail.com>
2025-12-11 21:20:22 +0600

    outer join

--- relational_query/urls.py
+++ relational_query/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('products/', views.products),
     path('inner_join/', views.inner_join),
+    path("outer_join/", views.outer_join),
 ]
--- relational_query/views.py
+++ relational_query/views.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 
-from relational_query.models import Product
+from relational_query.models import Product, Category
 
 
 def products(request):
@@ -9,8 +9,16 @@ def products(request):
     return JsonResponse({'products': list(products)})
 
 def inner_join(request):
-    #Product > Catagory (inner join)
+    #Product > Category (inner join)
     products = Product.objects.select_related('category').values(
         'id', 'name', 'price', 'category__id', 'category__name')
 
     return JsonResponse({'products': list(products)})
+
+
+def outer_join(request):
+    #Category > Product (outer join)
+    categories = Category.objects.prefetch_related('products').values(
+        'id', 'name','products__id', 'products__name', 'products__price')
+
+    return JsonResponse({'Categorys': list(categories)})
